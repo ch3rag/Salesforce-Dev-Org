c2a7af243d85901580b603445d8190155375ad37
Bharat Singh Rajput <bharatsr1997@gmail.com>
2023-08-04 13:49:34 +0530

    Update

--- force-app/main/default/aura/word-shuffle/aura/BoardPanel/BoardPanelHelper.js
+++ force-app/main/default/aura/word-shuffle/aura/BoardPanel/BoardPanelHelper.js
@@ -1,29 +1,32 @@
 ({
-	addResultRecord: function(component, gameResult) {
-		const action = component.get('c.addResult');
-		const modeValue = component.get('v.selectedMode').toUpperCase();
-		action.setParams({
-			result: gameResult,
-			mode: modeValue,
-		});
+    'addResultRecord': function (component, gameResult) {
+        const action = component.get('c.addResult');
+        const modeValue = component.get('v.selectedMode').toUpperCase();
 
-		action.setCallback(this, (response) => {
-			const state = response.getState();
-			if (state !== 'SUCCESS') {
-				console.log('Error in saving record.');
-			}
-		});
+        action.setParams({
+            'result': gameResult,
+            'mode': modeValue
+        });
 
-		$A.enqueueAction(action);
-	},
+        action.setCallback(this, (response) => {
+            const state = response.getState();
 
-	showToast : function(title, message, type) {
-		var toastEvent = $A.get("e.force:showToast");
-		toastEvent.setParams({
-			'title': title,
-			'message': message,
-			'type': type,
-		});
-		toastEvent.fire();
-	}
+            if (state !== 'SUCCESS') {
+                this.showToast('Error', 'Error in saving record', 'error');
+            }
+        });
+
+        $A.enqueueAction(action);
+    },
+
+    'showToast': function (title, message, type) {
+        const toastEvent = $A.get('e.force:showToast');
+
+        toastEvent.setParams({
+            'title': title,
+            'message': message,
+            'type': type
+        });
+        toastEvent.fire();
+    }
 });
