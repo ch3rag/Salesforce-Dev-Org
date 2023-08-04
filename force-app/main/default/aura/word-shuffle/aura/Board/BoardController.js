8416d711075fa677a6302605e0554872489ce5b5
Bharat Singh Rajput <bharatsr1997@gmail.com>
2023-08-04 14:24:15 +0530

    update

--- force-app/main/default/aura/word-shuffle/aura/Board/BoardController.js
+++ force-app/main/default/aura/word-shuffle/aura/Board/BoardController.js
@@ -1,47 +1,54 @@
 ({
-	onInit: function (component, event, helper) {
-		const gameMode = component.get('v.gameMode');
-		let column = 0;
-		if (gameMode === 'hard') {
-			column = 6;
-		} else if (gameMode == 'medium') {
-			column = 4;
-		} else {
-			column = 3;
-		}
-		const blockSize = 12 / column;
+    'onInit': function (component, event, helper) {
+        const gameMode = component.get('v.gameMode');
+        let columnCount = {
+            'easy': 3,
+            'medium': 4,
+            'hard': 6
+        };
+        let gridWidth = 12;
+        let numColumns = columnCount[gameMode];
 
-		const words = helper.getWords(column * column);
-		const winWord = helper.getWinWord(words);
-		component.set('v.words', words);
-		component.set('v.winWord', winWord);
-		component.set('v.blockSize', blockSize);
-		helper.resetBoard(component, event);
-	},
+        const blockSize = gridWidth / numColumns;
+        const words = helper.getWords(numColumns * numColumns);
+        const winWord = helper.getWinWord(words);
 
-	onRender: function (component, event, helper) {
-		// helper.resetBlocks(component);
-	},
+        component.set('v.words', words);
+        component.set('v.winWord', winWord);
+        component.set('v.blockSize', blockSize);
+        helper.resetBoard(component, event);
+    },
 
-	blockClickHandler: function(component, event, helper) {
-		let clickCount = component.get('v.clickCount') + 1;
-		const value = event.getParam('value');
-		if (value === component.get('v.winWord')) {
-			component.set('v.result', "You Won!");
-			helper.disableBoard(component);
-			helper.fireResultEvent('Win');
-		} else if (clickCount === 3) {
-			component.set('v.result', 'You Lost :(');
-			helper.disableBoard(component);
-			helper.fireResultEvent('Lose');
-		}
-		component.set('v.clickCount', clickCount);
-	},
+    'blockClickHandler': function (component, event, helper) {
+        const NUM_MAX_TURNS = 3;
 
-	reshuffleBoard: function(component, event, helper) {
-		const words = component.get('v.words');
-		const randomizedWords = helper.randomizeArray(words);
-		component.set('v.words', words);
-		helper.resetBoard(component, event);
-	}
+        let clickCount = component.get('v.clickCount');
+
+        // Increment Click Count
+        /* eslint-disable-next-line no-magic-numbers */
+        clickCount += 1;
+
+        const value = event.getParam('value');
+
+        if (value === component.get('v.winWord')) {
+            component.set('v.result', 'You Won!');
+            helper.disableBoard(component);
+            helper.fireResultEvent('Win');
+        } else if (clickCount === NUM_MAX_TURNS) {
+            component.set('v.result', 'You Lost :(');
+            helper.disableBoard(component);
+            helper.fireResultEvent('Lose');
+        }
+
+        component.set('v.clickCount', clickCount);
+    },
+
+    'reshuffleBoard': function (component, event, helper) {
+        const words = component.get('v.words');
+
+        helper.randomizeArray(words);
+
+        component.set('v.words', words);
+        helper.resetBoard(component, event);
+    }
 });
